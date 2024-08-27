1e5dd276b56b451fe61db5f152b506dff7926a16
EduardoTurci2109 <134058449+EduardoTurci2109@users.noreply.github.com>
2024-08-27 17:32:14 -0300

    Add files via upload

---
+++ missao antibullying 2.0/missao antibullying 2.0/js/perguntas.js
@@ -0,0 +1,147 @@
+export const perguntas = [
+    {
+        enunciado: "você está saindo da escola e vê um grupo de alunos discutindo sobre a importância de combater o bullying. Qual é a sua primeira reação?",
+        alternativas: [
+            {
+                texto: "Isso é preocupante!",
+                afirmacao: [
+                    "Ficou preocupado com a forma como o bullying pode afetar os alunos.",
+                    "Achou preocupante ver que o bullying ainda é um problema significativo nas escolas."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Isso é inspirador!",
+                afirmacao: [
+                    "Quer saber mais sobre como combater o bullying de forma eficaz.",
+                    "Pensa que iniciativas contra o bullying são importantes para criar um ambiente escolar mais seguro e respeitoso."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Após uma aula sobre bullying, o professor pede que você escreva um trabalho sobre estratégias para prevenir o bullying na escola. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza recursos online e literatura sobre prevenção do bullying para escrever seu trabalho, explicando de forma clara e acessível.",
+                afirmacao: [
+                    "onseguiu encontrar informações úteis e confiáveis sobre prevenção do bullying.",
+                    "Percebeu que existem muitas estratégias eficazes para lidar com o bullying e decidiu incluir essas estratégias no seu trabalho."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Escreve o trabalho com base em conversas com colegas, algumas pesquisas e seus próprios conhecimentos sobre o tema.",
+                afirmacao: [
+                    "Sentiu que a pesquisa e as discussões com colegas ajudaram a enriquecer seu trabalho.",
+                    "Achou que utilizar métodos mais tradicionais e interativos foi mais produtivo para entender e abordar o tema do bullying."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Após uma aula sobre bullying, o professor pede que você escreva um trabalho sobre estratégias para prevenir o bullying na escola. Qual atitude você toma?",
+        alternativas: [
+            {
+                texto: "Utiliza recursos online e literatura sobre prevenção do bullying para escrever seu trabalho, explicando de forma clara e acessível.",
+                afirmacao: [
+                    "Conseguiu encontrar informações úteis e confiáveis sobre prevenção do bullying.",
+                    "Percebeu que existem muitas estratégias eficazes para lidar com o bullying e decidiu incluir essas estratégias no seu trabalho."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Escreve o trabalho com base em conversas com colegas, algumas pesquisas e seus próprios conhecimentos sobre o tema.",
+                afirmacao: [
+                    "Sentiu que a pesquisa e as discussões com colegas ajudaram a enriquecer seu trabalho.",
+                    "Achou que utilizar métodos mais tradicionais e interativos foi mais produtivo para entender e abordar o tema do bullying."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Discussões sobre bullying podem revelar muito sobre a cultura escolar. Agora imagine que você precisa participar de um debate sobre o impacto do bullying no bem-estar dos alunos. Qual posicionamento você adotaria?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que o combate ao bullying pode melhorar significativamente o ambiente escolar e o bem-estar dos alunos.",
+                afirmacao: [
+                    "Acredita que criar um ambiente seguro e respeitoso ajuda na saúde mental dos alunos.",
+                    "Participa ativamente em campanhas e atividades que visam reduzir o bullying na escola."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com a forma como o bullying pode afetar negativamente o bem-estar dos alunos e defendo a necessidade de medidas eficazes para enfrentá-lo.",
+                afirmacao: [
+                    "Sua preocupação motivou você a promover discussões sobre a importância do respeito e da empatia na escola.",
+                    "Criou ou participou de grupos de apoio para ajudar vítimas de bullying e promover um ambiente mais inclusivo."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Discussões sobre bullying podem revelar muito sobre a cultura escolar. Agora imagine que você precisa participar de um debate sobre o impacto do bullying no bem-estar dos alunos. Qual posicionamento você adotaria?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que o combate ao bullying pode melhorar significativamente o ambiente escolar e o bem-estar dos alunos.",
+                afirmacao: [
+                    "Acredita que criar um ambiente seguro e respeitoso ajuda na saúde mental dos alunos.",
+                    "Participa ativamente em campanhas e atividades que visam reduzir o bullying na escola."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com a forma como o bullying pode afetar negativamente o bem-estar dos alunos e defendo a necessidade de medidas eficazes para enfrentá-lo.",
+                afirmacao: [
+                    "Sua preocupação motivou você a promover discussões sobre a importância do respeito e da empatia na escola.",
+                    "Criou ou participou de grupos de apoio para ajudar vítimas de bullying e promover um ambiente mais inclusivo."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "O combate ao bullying é essencial para garantir um ambiente escolar saudável. Após uma discussão em sala sobre o impacto do bullying, você participa de um debate sobre como prevenir e combater o bullying. Como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a prevenção e a educação são as melhores ferramentas para combater o bullying.",
+                afirmacao: [
+                    "Acredita que educar os alunos sobre empatia e respeito pode reduzir significativamente o bullying.",
+                    "Participa de programas e atividades que promovem a conscientização sobre o impacto do bullying."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com a eficácia das medidas atuais e defendo a necessidade de novas estratégias para combater o bullying de forma mais eficiente.",
+                afirmacao: [
+                    "Se preocupa com as abordagens existentes e busca maneiras de aprimorar a prevenção do bullying.",
+                    "Participa de fóruns e grupos de discussão para encontrar novas soluções para o problema do bullying."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Após a discussão sobre o impacto do bullying, você precisa criar uma apresentação visual para representar suas ideias sobre a prevenção do bullying. O que você faz?",
+        alternativas: [
+            {
+                texto: "Cria uma apresentação utilizando ferramentas tradicionais de design, como o PowerPoint.",
+                afirmacao: [
+                    "Percebeu que muitos alunos ainda têm dificuldade com ferramentas digitais e decidiu compartilhar dicas para criar apresentações eficazes.",
+                    "Acredita que métodos tradicionais ainda são importantes para expressar suas ideias e criar impacto."
+                ],
+            },
+            {
+                texto: "Cria uma apresentação utilizando uma plataforma online de design que gera visualizações e gráficos.",
+                afirmacao: [
+                    "Encontrou que essas ferramentas aceleram o processo de criação e ajudam a comunicar suas ideias de forma clara e visual.",
+                    "Compartilhou suas apresentações em redes sociais para ajudar outros a entender melhor o tema da prevenção do bullying."
+                ],
+            },
+        ]
+    }
+];
